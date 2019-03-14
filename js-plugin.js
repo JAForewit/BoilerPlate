6f1361ed2b201bb0bc750d67a6b966f180f6be48
Marc Anderson <marcda495@gmail.com>
2019-03-14 15:50:19 -0400

    Create js-plugin.js

---
+++ js-plugin.js
@@ -0,0 +1,32 @@
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else {
+        root.MyName = factory();
+    }
+}(this, function () {
+
+    'use strict';
+
+    // PROPERTIES
+    var defaults = {
+    }
+
+    // PRIVATE FUNCTIONS
+
+    /*
+    usage:
+
+    new MyName (element, options)
+      - or -
+    new MyName (element)
+    */
+    function MyName(element, options) {
+        var me = this;
+
+    }
+
+    return MyName;
+}));
